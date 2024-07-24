7fcab321331e5b5371a2a80abfeffcd25cbac8f8
Sakshi Tiwari <96986663+sakshi8707@users.noreply.github.com>
2024-07-24 11:44:51 +0530

    added till city view

--- src/Components/CityItem.jsx
+++ src/Components/CityItem.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./CityItem.module.css";
 import { Link } from "react-router-dom";
+import { useCities } from "../Contexts/CitiesContext";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -21,12 +22,18 @@ const flagemojiToPNG = (flag) => {
 
 function CityItem({ city }) {
   const { cityName, emoji, date, id, position } = city;
-  // console.log();
+  const { latestVisitedCity } = useCities();
+
+  const isActive = latestVisitedCity && latestVisitedCity.id === city.id;
+
   return (
-    <li className={styles.cityItemWrapper}>
+    <li
+      className={`${styles.cityItemWrapper} ${
+        isActive ? styles.cityItemActive : ""
+      }`}
+    >
       <Link
-          className={styles.cityItem}
-          // to={`/${id}?lat=${position.lat}&lng=${position.lng}`}
+        className={styles.cityItem}
         to={`/app/cities/${id}?lat=${position.lat}&lng=${position.lng}`}
       >
         <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
