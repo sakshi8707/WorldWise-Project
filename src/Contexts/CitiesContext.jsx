8de1b9f5661d9af118a5c2090e313c816e20a305
Sakshi Tiwari <96986663+sakshi8707@users.noreply.github.com>
2024-07-24 11:43:11 +0530

    added city views

--- src/Contexts/CitiesContext.jsx
+++ src/Contexts/CitiesContext.jsx
@@ -1,15 +1,21 @@
-import { createContext , useEffect , useState } from "react";
+import {
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+  useCallback,
+} from "react";
 
-  
 const BASE_URL = "http://localhost:9000";
 const CitiesContext = createContext();
 
-function CitiesProvider({children}) {
-  
+function CitiesProvider({ children }) {
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [currentCity, setCurrentCity] = useState({});
+  const [latestVisitedCity, setLatestVisitedCity] = useState(null);
 
-  useEffect(function () {
+  useEffect(() => {
     async function fetchCities() {
       try {
         setIsLoading(true);
@@ -24,4 +30,40 @@ function CitiesProvider({children}) {
     }
     fetchCities();
   }, []);
+
+  const getCity = useCallback(async (id) => {
+    try {
+      setIsLoading(true);
+      const res = await fetch(`${BASE_URL}/cities/${id}`);
+      const data = await res.json();
+      setCurrentCity(data);
+    } catch {
+      alert("There was an error loading data...");
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  return (
+    <CitiesContext.Provider
+      value={{
+        cities,
+        isLoading,
+        currentCity,
+        latestVisitedCity,
+        getCity,
+      }}
+    >
+      {children}
+    </CitiesContext.Provider>
+  );
 }
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext was used outside its provider");
+  return context;
+}
+
+export { CitiesProvider, useCities };
