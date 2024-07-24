05d687c0d65ffa16a455b231cb3c119d3d08e831
Sakshi Tiwari <96986663+sakshi8707@users.noreply.github.com>
2024-07-24 11:50:38 +0530

    added cities views

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Product from "./pages/Product";
 import PricingPage from "./pages/Pricing";
 import HomePage from "./pages/Homepage";
@@ -11,15 +10,11 @@ import City from "./Components/City.jsx";
 import CountryList from "./Components/CountryList.jsx";
 import Form from "./Components/Form";
 import Map from "./Components/Map";
-import {  Navigate } from "react-router-dom";
-
-
+import { CitiesProvider } from "./Contexts/CitiesContext.jsx";
 
 function App() {
-    
-
   return (
-    <div>
+    <CitiesProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
@@ -27,24 +22,17 @@ function App() {
           <Route path="pricing" element={<PricingPage />} />
           <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
-            {/* Nested routes under AppLayout */}
             <Route index element={<Navigate replace to="cities" />} />
-            <Route
-              path="cities"
-              element={<CityList cities={cities} isLoading={isLoading} />}
-            />
+            <Route path="cities" element={<CityList />} />
             <Route path="cities/:id" element={<City />} />
-            <Route
-              path="countries"
-              element={<CountryList cities={cities} isLoading={isLoading} />}
-            />
+            <Route path="countries" element={<CountryList />} />
             <Route path="form" element={<Form />} />
             <Route path="map" element={<Map />} />
           </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </CitiesProvider>
   );
 }
 
