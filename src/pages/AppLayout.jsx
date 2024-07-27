033a4f68797728ae49cc989842fd5eb7c622b41b
Sakshi Tiwari <96986663+sakshi8707@users.noreply.github.com>
2024-07-27 20:36:49 +0530

    Added MarkerFor Each city

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,15 +1,23 @@
 import Map from "../Components/Map";
 import Sidebar from "../Components/Sidebar";
 import styles from "./AppLayout.module.css";
+import { useCities } from "../Contexts/CitiesContext";
+
+
 function AppLayout() {
+
+   const { cities } = useCities();
+
+  
   return (
     <div className={styles.app}>
       <Sidebar />
       <div className={styles.mapContainer}>
-        <Map />
+        <Map cities={cities} />
       </div>
     </div>
   );
 }
 
 export default AppLayout;
+
