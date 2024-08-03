1c823110aec98b53c19a749ca7b64f423b301523
Sakshi Tiwari <96986663+sakshi8707@users.noreply.github.com>
2024-08-03 14:33:22 +0530

    updated  hooks file

--- src/Contexts/CitiesContext.jsx
+++ src/Contexts/CitiesContext.jsx
@@ -43,76 +43,22 @@ function CitiesProvider({ children }) {
       setIsLoading(false);
     }
   }, []);
+ 
 
-  return (
-    <CitiesContext.Provider
-      value={{
-        cities,
-        isLoading,
-        currentCity,
-        latestVisitedCity,
-        getCity,
-      }}
-    >
-      {children}
-    </CitiesContext.Provider>
-  );
-}
-
-function useCities() {
-  const context = useContext(CitiesContext);
-  if (context === undefined)
-    throw new Error("CitiesContext was used outside its provider");
-  return context;
-}
-
-export { CitiesProvider, useCities };
-import {
-  createContext,
-  useContext,
-  useEffect,
-  useState,
-  useCallback,
-} from "react";
-
-const BASE_URL = "http://localhost:9000";
-const CitiesContext = createContext();
-
-function CitiesProvider({ children }) {
-  const [cities, setCities] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentCity, setCurrentCity] = useState({});
-  const [latestVisitedCity, setLatestVisitedCity] = useState(null);
-
-  useEffect(() => {
-    async function fetchCities() {
-      try {
-        setIsLoading(true);
-        const res = await fetch(`${BASE_URL}/cities`);
-        const data = await res.json();
-        setCities(data);
-      } catch {
-        alert("There was an error loading data...");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, []);
-
-  const getCity = useCallback(async (id) => {
+  async function deleteCity(id) {
     try {
       setIsLoading(true);
-      const res = await fetch(`${BASE_URL}/cities/${id}`);
-      const data = await res.json();
-      setCurrentCity(data);
+      await fetch(`$(BASE_URL)/cities/${id}`, {
+        method: "DELETE",
+      });
+
+      setCities((cities) => cities.filter((city)=>city.id !== id));
     } catch {
-      alert("There was an error loading data...");
+      alert("There was an error deleting city.");
     } finally {
       setIsLoading(false);
     }
-  }, []);
-
+  }
   return (
     <CitiesContext.Provider
       value={{
@@ -121,6 +67,7 @@ function CitiesProvider({ children }) {
         currentCity,
         latestVisitedCity,
         getCity,
+        deleteCity,
       }}
     >
       {children}
