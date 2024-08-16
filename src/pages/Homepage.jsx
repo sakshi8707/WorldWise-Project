62c085ff32224346ff78ca9088239e4c4b4e5e22
Sakshi Tiwari <96986663+sakshi8707@users.noreply.github.com>
2024-08-16 22:04:34 +0530

    Changed Link Tracking

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,12 @@
-import styles from "./Homepage.module.css";
 import { Link } from "react-router-dom";
 import PageNav from "../Components/PageNav";
+import styles from "./Homepage.module.css";
 
 export default function Homepage() {
   return (
     <main className={styles.homepage}>
       <PageNav />
+
       <section>
         <h1>
           You travel the world.
@@ -17,8 +18,8 @@ export default function Homepage() {
           of. Never forget your wonderful experiences, and show your friends how
           you have wandered the world.
         </h2>
-        <Link to="/app" className="cta">
-          start tracking now
+        <Link to="/login" className="cta">
+          Start tracking now
         </Link>
       </section>
     </main>
