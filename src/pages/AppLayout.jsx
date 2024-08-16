3dda49b23a4e405eb679f1b603ec2116ddec3929
Sakshi Tiwari <96986663+sakshi8707@users.noreply.github.com>
2024-08-16 22:03:06 +0530

    Imported User.jsx

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,23 +1,22 @@
 import Map from "../Components/Map";
 import Sidebar from "../Components/Sidebar";
 import styles from "./AppLayout.module.css";
+import User from "../Components/User";
 import { useCities } from "../Contexts/CitiesContext";
 
-
 function AppLayout() {
+  const { cities } = useCities();
 
-   const { cities } = useCities();
-
-  
   return (
     <div className={styles.app}>
       <Sidebar />
+      
       <div className={styles.mapContainer}>
         <Map cities={cities} />
       </div>
+      {/* <User /> */}
     </div>
   );
 }
 
 export default AppLayout;
-
