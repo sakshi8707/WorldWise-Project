0c0b18c4021eab74ff891d30511e5437cacb2ac3
Sakshi Tiwari <96986663+sakshi8707@users.noreply.github.com>
2024-09-01 19:37:10 +0530

    Added Carousel Features

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,63 +1,108 @@
-.product {
-  margin: 2.5rem;
-  padding: 2.5rem 5rem;
-  background-color: var(--color-dark--1);
-  min-height: calc(100vh - 5rem);
+import { Carousel } from "react-responsive-carousel";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import styles from "./Product.module.css";
+import PageNav from "../Components/PageNav";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const CustomArrow = ({ type, onClick }) => {
+  const isNext = type === "next";
+  return (
+    <button
+      onClick={onClick}
+      className={`${styles.customArrow} ${isNext ? styles.next : styles.prev}`}
+    >
+      <FontAwesomeIcon
+        icon={isNext ? faChevronRight : faAngleLeft}
+        style={{ color: "#00c46a", fontSize: "26px" }}
+        className={styles.iconSize} 
+      />
+    </button>
+  );
+};
+
+export default function Product() {
+  return (
+    <main className={styles.product}>
+      <PageNav />
+      <Carousel
+        showStatus={false}
+        renderArrowNext={(onClickHandler) => (
+          <CustomArrow type="next" onClick={onClickHandler} />
+        )}
+        renderArrowPrev={(onClickHandler) => (
+          <CustomArrow type="prev" onClick={onClickHandler} />
+        )}
+      >
+        <div className={styles.slide}>
+          <section>
+            <img
+              src="img-1.jpg"
+              alt="person with dog overlooking mountain with sunset"
+            />
+            <div className={styles.overlay}>
+              <h2>About WorldWise.</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+                est dicta illum vero culpa cum quaerat architecto sapiente eius
+                non soluta, molestiae nihil laborum, placeat debitis, laboriosam
+                at fuga perspiciatis?
+              </p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Corporis doloribus libero sunt expedita ratione iusto, magni, id
+                sapiente sequi officiis et.
+              </p>
+            </div>
+          </section>
+        </div>
+
+        <div className={styles.slide}>
+          <section>
+            <img
+              src="img-2.jpg"
+              alt="person with dog overlooking mountain with sunset"
+            />
+            <div className={styles.overlay}>
+              <h2>About WorldWise.</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+                est dicta illum vero culpa cum quaerat architecto sapiente eius
+                non soluta, molestiae nihil laborum, placeat debitis, laboriosam
+                at fuga perspiciatis?
+              </p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Corporis doloribus libero sunt expedita ratione iusto, magni, id
+                sapiente sequi officiis et.
+              </p>
+            </div>
+          </section>
+        </div>
+
+        <div className={styles.slide}>
+          <section>
+            <img
+              src="img-1.jpg"
+              alt="person with dog overlooking mountain with sunset"
+            />
+            <div className={styles.overlay}>
+              <h2>About WorldWise.</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+                est dicta illum vero culpa cum quaerat architecto sapiente eius
+                non soluta, molestiae nihil laborum, placeat debitis, laboriosam
+                at fuga perspiciatis?
+              </p>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Corporis doloribus libero sunt expedita ratione iusto, magni, id
+                sapiente sequi officiis et.
+              </p>
+            </div>
+          </section>
+        </div>
+      </Carousel>
+    </main>
+  );
 }
-
-.product section {
-  width: clamp(80rem, 80%, 90rem);
-  margin: 6rem auto;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 7rem;
-  align-items: center;
-}
-
-.product img {
-  width: 100%;
-}
-
-.product h2 {
-  font-size: 4rem;
-  line-height: 1.2;
-  margin-bottom: 3rem;
-}
-
-.product p {
-  font-size: 1.6rem;
-  margin-bottom: 2rem;
-}
-
-.product section a {
-  margin-top: 2rem;
-}
-
-/* ------------Carousel styling---------- */
-
-.customArrow {
-  background: transparent;
-  border: none;
-  cursor: pointer;
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  z-index: 10;
-}
-
-.customArrow img {
-  width: 0rem; /* Adjust size as needed */
-  height: auto;
-  fill: var(--color-dark--2);
-}
-
-.next {
-  right: 1rem; 
-  width: 4rem;
-}
-
-.prev {
-  left: 1rem; 
-  width: 4rem;
-}
-
